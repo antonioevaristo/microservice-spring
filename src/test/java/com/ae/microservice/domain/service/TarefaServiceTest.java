2263f8763017aa4f115d5291e64ba154dc17dfb7
Antonio Evaristo <antonio.c.e.barbosa@gmail.com>
2024-03-09 22:47:38 -0300

    testes unitarios concluidos, não vou cobrir 100%

--- src/test/java/com/ae/microservice/domain/service/TarefaServiceTest.java
+++ src/test/java/com/ae/microservice/domain/service/TarefaServiceTest.java
@@ -12,9 +12,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
-import java.util.Calendar;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -53,12 +51,63 @@ class TarefaServiceTest {
                 .hasMessageContaining("Tarefa já cadastrada.");
     }
 
+    @Test
+    void update_status_tarefa_sucesso() throws TarefaException {
+        Tarefa tarefa = getTarefa();
+        Mockito.when(tarefaRepository.get(tarefa.getCodigo())).thenReturn(Optional.of(tarefa));
+        Mockito.when(tarefaRepository.create(tarefa)).thenReturn(tarefa);
+
+        Tarefa tarefa1 = tarefaService.updateStatus(tarefa, Boolean.TRUE);
+        assertThat(tarefa1.getStatus()).isTrue();
+    }
+    @Test
+    void update_status_tarefa_exception() throws TarefaException {
+        Tarefa tarefa = getTarefa();
+        Mockito.when(tarefaRepository.get(tarefa.getCodigo())).thenReturn(Optional.empty());
+        assertThatThrownBy(() -> tarefaService.updateStatus(tarefa,Boolean.TRUE))
+                .isInstanceOf(TarefaException.class)
+                .hasMessageContaining("Tarefa não existe");
+
+    }
+
+    @Test
+    void getAll_tarefas_prioridade_alta(){
+        List<Tarefa> tarefas = Arrays.asList(tarefa,getTarefa(PrioridadeEnum.ALTA));
+        Mockito.when(tarefaRepository.getAll()).thenReturn(tarefas);
+
+        List<Tarefa> all = tarefaService.getAll(PrioridadeEnum.ALTA, Boolean.FALSE);
+        assertThat(all).isNotEmpty();
+        assertThat(all.size()).isEqualTo(1);
+        assertThat(all.get(0).getPrioridade()).isEqualTo(PrioridadeEnum.ALTA);
+        assertThat(all.get(0).getStatus()).isEqualTo(Boolean.FALSE);
+
+    }
+    @Test
+    void getAll_tarefas_status_true(){
+        List<Tarefa> tarefas = Arrays.asList(tarefa,getTarefa(PrioridadeEnum.ALTA));
+        Mockito.when(tarefaRepository.getAll()).thenReturn(tarefas);
+
+        List<Tarefa> all = tarefaService.getAll(PrioridadeEnum.BAIXA, Boolean.TRUE);
+        assertThat(all).isNotEmpty();
+        assertThat(all.size()).isEqualTo(1);
+        assertThat(all.get(0).getPrioridade()).isEqualTo(PrioridadeEnum.BAIXA);
+        assertThat(all.get(0).getStatus()).isEqualTo(Boolean.TRUE);
+    }
+
     private static Tarefa getTarefa() {
+        Tarefa tarefa = new Tarefa();
+        tarefa.setCodigo(UUID.randomUUID());
+        tarefa.setStatus(Boolean.TRUE);
+        tarefa.setDescricao("Acordar cedo");
+        tarefa.setPrioridade(PrioridadeEnum.BAIXA);
+        tarefa.setHorarioTarefa(Calendar.getInstance().getTime());
+        return tarefa;
+    }private static Tarefa getTarefa(PrioridadeEnum prioridadeEnum) {
         Tarefa tarefa = new Tarefa();
         tarefa.setCodigo(UUID.randomUUID());
         tarefa.setStatus(Boolean.FALSE);
         tarefa.setDescricao("Acordar cedo");
-        tarefa.setPrioridade(PrioridadeEnum.ALTA);
+        tarefa.setPrioridade(prioridadeEnum);
         tarefa.setHorarioTarefa(Calendar.getInstance().getTime());
         return tarefa;
     }
